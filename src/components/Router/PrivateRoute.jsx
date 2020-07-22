8898d511cd986d906eef6f63a1bd26a813654cc6
Juan Cruz Vieiro <jvieiro@atixlabs.com>
2020-07-21 21:18:44 -0300

    navbar y algunas utils

--- src/components/Router/PrivateRoute.jsx
+++ src/components/Router/PrivateRoute.jsx
@@ -1,39 +1,46 @@
-import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
-import { Route, Redirect } from 'react-router-dom';
-import { UserContext } from '../../services/providers/user-context.jsx';
-//import Header from '../molecules/header/header';
-import { LOGIN_URL, HOME_URL } from '../../utils/constants';
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import { Route, Redirect } from "react-router-dom";
+import { UserContext } from "../../services/providers/user-context.jsx";
+import Header from "../molecules/Header/header";
+import { LOGIN_URL, HOME_URL } from "../../utils/constants";
 
-const PrivateRoute = ({ component: Component, requireAuthentication, ...rest }) => {
+const PrivateRoute = ({
+  component: Component,
+  requireAuthentication,
+  ...rest
+}) => {
   const { user } = useContext(UserContext);
   const getComponent = () =>
-    rest.path === '/' ? (
+    rest.path === "/" ? (
       <Redirect to={HOME_URL} />
     ) : (
-        <div>
-          {/*<Header /> */ }
-          <Route {...rest} render={properties => <Component {...properties} />} />
-        </div>
-      );
+      <div>
+        <Header />
+        <Route
+          {...rest}
+          render={(properties) => <Component {...properties} />}
+        />
+      </div>
+    );
 
-  if (!requireAuthentication) {
-    return <Route {...rest} render={properties => <Component {...properties} />} />;
-  }
+  if (!requireAuthentication)
+    return (
+      <Route {...rest} render={(properties) => <Component {...properties} />} />
+    );
 
   return user.accessToken ? getComponent() : <Redirect to={LOGIN_URL} />;
-  //return <Redirect to={LOGIN_URL} />;
 };
 
 PrivateRoute.propTypes = {
   component: PropTypes.func.isRequired,
   exact: PropTypes.bool,
   path: PropTypes.string,
-  requireAuthentication: PropTypes.bool.isRequired
+  requireAuthentication: PropTypes.bool.isRequired,
 };
 
 PrivateRoute.defaultProps = {
-  exact: false
+  exact: false,
 };
 
 export default PrivateRoute;
---
+++ src/components/molecules/Navbar/navbar.jsx
@@ -0,0 +1,90 @@
+import React, { useState, useContext } from "react";
+import { Menu, Icon, message } from "antd";
+import "./_style.scss";
+import PropTypes from "prop-types";
+import { UserContext } from "../../../services/providers/user-context";
+import { compare } from "../../../utils/func-helpers";
+import { LOGIN_URL } from "../../../utils/constants";
+import { useRedirect } from "../../Router/redirect";
+import { processedErrorMessage } from "../../../services/api-calls/helpers";
+import apiCalls from "../../../services/api-calls/all";
+
+const { logoutRequest } = apiCalls();
+
+const { SubMenu } = Menu;
+
+const Navbar = ({ navbarEntries }) => {
+  const [current, setCurrent] = useState("");
+  const { setUser } = useContext(UserContext);
+  const { redirect, setUrlToRedirect } = useRedirect();
+
+  const signOut = async () => {
+    try {
+      await logoutRequest();
+    } catch (error) {
+      const errorMessage = processedErrorMessage(error);
+      message.error(errorMessage);
+    }
+    setUser(null);
+    setUrlToRedirect(LOGIN_URL);
+  };
+
+  const itemClick = (path) => {
+    if (path === LOGIN_URL) signOut();
+    else setUrlToRedirect(path);
+  };
+
+  const menuItemsRenderer = (items) =>
+    items
+      .sort((a, b) => compare(a, b, "order"))
+      .map((item) => (
+        <Menu.Item key={item.key}>
+          <button onClick={() => itemClick(item.url)}>
+            {item.icon && <Icon type={item.icon} />}
+            {item.description}
+          </button>
+        </Menu.Item>
+      ));
+
+  const menuItems = navbarEntries
+    .sort((a, b) => compare(a, b, "order"))
+    .map((entry) =>
+      entry.items ? (
+        <SubMenu
+          key={entry.key}
+          title={
+            <span className="submenu-title-wrapper">
+              <button onClick={() => itemClick(entry.url)}>
+                {entry.icon && <Icon type={entry.icon} />}
+                {entry.description}
+              </button>
+            </span>
+          }
+        >
+          {menuItemsRenderer(entry.items)}
+        </SubMenu>
+      ) : (
+        menuItemsRenderer([entry])
+      )
+    );
+
+  return (
+    <div>
+      {redirect()}
+      <Menu
+        onClick={({ key }) => setCurrent(key)}
+        selectedKeys={[current]}
+        mode="horizontal"
+        className={"ulMain"}
+      >
+        {menuItems}
+      </Menu>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  navbarEntries: PropTypes.any,
+};
+
+export default Navbar;
