f3f394ffc4e24bf00db333e09e79adc77f6979ea
juanvieiro <jvieirocorallo@est.frba.utn.edu.ar>
2020-09-13 21:57:49 -0300

    add weather images and modal logic

--- src/components/atoms/CustomDatePicker/CustomDatePicker.jsx
+++ src/components/atoms/CustomDatePicker/CustomDatePicker.jsx
@@ -4,14 +4,15 @@ import { DatePicker } from 'antd';
 import PropTypes from 'prop-types';
 import { isNowOlderThan } from '../../../utils/func-helpers';
 
-const CustomDatePicker = ({ action, placeholder, theme }) => {
+const now = moment();
+
+const CustomDatePicker = ({ action, placeholder, theme, defaultDate }) => {
   const handleChange = (date, dateString) => action(dateString);
   const dateFormat = 'DD/MM/YYYY';
-  const now = moment();
   return (
     <DatePicker
       onChange={handleChange}
-      defaultValue={now}
+      defaultValue={defaultDate}
       placeholder={placeholder}
       className={theme}
       format={dateFormat}
@@ -22,8 +23,14 @@ const CustomDatePicker = ({ action, placeholder, theme }) => {
 
 CustomDatePicker.propTypes = {
   action: PropTypes.func.isRequired,
-  placeholder: PropTypes.string.isRequired,
-  theme: PropTypes.string
+  placeholder: PropTypes.string,
+  theme: PropTypes.string,
+  defaultDate : PropTypes.object
+};
+
+CustomDatePicker.defaultProps={
+ placeholder: "Fecha",
+ defaultValue: now
 };
 
 export default CustomDatePicker;
--- src/components/molecules/DayModal/DayModal.jsx
+++ src/components/molecules/DayModal/DayModal.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Button } from 'antd';
 import './_style.scss';
+import { dateHandler } from '../../../utils/func-helpers';
+import CustomDatePicker from '../../atoms/CustomDatePicker/CustomDatePicker';
+import NewsEventsTable from '../NewsEventsTable/NewsEventsTable';
 
 const DayModal = ({ defaultValue, visible, setVisible }) => {
+  const [day, setDay] = useState();
+  const { yesterday, tomorrow, format } = dateHandler;
+
+  useEffect(() => {
+    setDay(defaultValue);
+  }, [defaultValue]);
+
+  const handlePickerChange = value => value && setDay(value);
+
   return (
     <Modal
       title="Información del día"
       visible={visible}
+      onCancel={() => setVisible(false)}
       onOk={() => setVisible(false)}
       cancelButtonProps={{ style: { display: 'none' } }}
+      width={600}
     >
       <div>
-        <p className="date">Fecha</p>
-        <div className="currentDate">
-        <Button shape="circle" size="small">
-          {'<'}
-        </Button>
-        <span id="day">{defaultValue}</span>
-        <Button shape="circle" size="small">
-          {'>'}
-        </Button>
+        <div className="dateComponent">
+          <p className="date">Fecha</p>
+          <div className="datePicker">
+            <CustomDatePicker action={handlePickerChange} defaultDate={format(defaultValue)} />
+          </div>
+          <div className="currentDate">
+            <Button onClick={() => setDay(yesterday(day))} shape="circle" size="small">
+              {'<'}
+            </Button>
+            <span id="day">{day}</span>
+            <Button onClick={() => setDay(tomorrow(day))} shape="circle" size="small">
+              {'>'}
+            </Button>
+          </div>
+        </div>
+        <div className="newsTable">
+          <NewsEventsTable />
         </div>
       </div>
     </Modal>
