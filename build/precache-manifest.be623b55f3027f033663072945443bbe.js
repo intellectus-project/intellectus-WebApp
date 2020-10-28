2a8ee5ce3895a785c9c63f363f6dcc3d24fa906d
Eric <eric.stoppel@gmail.com>
2020-10-28 13:53:09 -0300

    build

---
+++ build/precache-manifest.be623b55f3027f033663072945443bbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c49819f3bd73395a324e6d79f19b7d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ef743dcd1bd2304047cc",
+    "url": "./static/css/2.5e21aa2c.chunk.css"
+  },
+  {
+    "revision": "83bcc90902138c64921c",
+    "url": "./static/css/main.70843dd7.chunk.css"
+  },
+  {
+    "revision": "ef743dcd1bd2304047cc",
+    "url": "./static/js/2.bc0e6dee.chunk.js"
+  },
+  {
+    "revision": "1e89a1fb5303f37d46610f4b765a6aa5",
+    "url": "./static/js/2.bc0e6dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83bcc90902138c64921c",
+    "url": "./static/js/main.cfe6f2f5.chunk.js"
+  },
+  {
+    "revision": "be8662b97192bdc8dc52",
+    "url": "./static/js/runtime-main.02f37acd.js"
+  }
+]);
