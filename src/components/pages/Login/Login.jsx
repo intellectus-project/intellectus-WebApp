a24a551eca63749eb721d81c7ab264bf11ddaa6c
juanvieiro <jvieirocorallo@est.frba.utn.edu.ar>
2020-10-28 23:02:20 -0300

    handle operator and admin session

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,29 +1,40 @@
-import React, { useContext, useState, useEffect, Component } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { UserContext } from '../../../services/providers/user-context';
 import { loginInputs } from '../../../utils/form_inputs/inputs-login';
 import apiCalls from '../../../services/api-calls/all';
 import AntForm from '../../molecules/ant-form';
 import { processedErrorMessage } from '../../../services/api-calls/helpers';
 import { useRedirect } from '../../Router/redirect';
-import { HOME_URL } from '../../../utils/constants';
+import { HOME_URL, OPERATOR, ROLE_VIEWER, ROLE_ADMIN, USERS_URL } from '../../../utils/constants';
 import './_style.scss';
 import '../../../css/app.scss';
 
 const { loginRequest } = apiCalls();
 
-const Login = () => {
+const Login = props => {
   const { user, setUser } = useContext(UserContext);
   const [errorMessage, setErrorMessage] = useState();
   const { redirect, setUrlToRedirect } = useRedirect();
 
   useEffect(() => {
-    if (user.accessToken) setUrlToRedirect(HOME_URL);
+    if (user.accessToken) {
+      switch (user.role) {
+        case ROLE_VIEWER:
+          props.history.push(`${OPERATOR}?id=${user.id}`);
+        case ROLE_ADMIN: {
+          return setUrlToRedirect(USERS_URL);
+        }
+        default:
+          setUrlToRedirect(HOME_URL);
+      }
+    }
   }, [user]);
 
   const login = async values => {
     delete Object.assign(values, { username: values.email }).email;
     try {
       const { data: logInData } = await loginRequest(values);
+      console.log('loginData ', logInData);
       setUser(logInData);
     } catch (error) {
       const errorMessage = processedErrorMessage(error);
